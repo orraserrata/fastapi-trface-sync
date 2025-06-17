f608d080558164580cf2349db2a797f65d96bb8f
orraserrata <honoriscausa.lostsaga@gmail.com>
2025-06-18 01:19:07 +0300

    zklib modülü ile geçiş yapıldı

--- zk_handler.py
+++ zk_handler.py
@@ -1,10 +1,7 @@
-from pyzk.zkmodules.zkbase import ZK
+from zklib import zklib
 
 def fetch_attendance_records():
-    zk = ZK("0.0.0.0", port=4370, timeout=5, force_udp=False)
-    conn = zk.connect()
-    conn.disable_device()
-    attendance = conn.get_attendance()
-    conn.enable_device()
-    conn.disconnect()
-    return [{"user_id": r.user_id, "timestamp": r.timestamp} for r in attendance]
+    zk = zklib.ZKLib(ip, port)
+zk.connect()
+attendances = zk.get_attendance()
+
